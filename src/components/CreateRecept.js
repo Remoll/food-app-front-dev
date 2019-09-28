ae2eb99eb61ac810e09cecc361505c3da1b89b2c
Remoll <remigiusz.slusarczyk@gmail.com>
2019-09-28 17:28:25 +0200

    improve ui design

--- src/components/CreateRecept.js
+++ src/components/CreateRecept.js
@@ -3,6 +3,8 @@ import '../style/CreateRecept.css'
 
 class CreateRecept extends Component {
     state = {
+        itemSelected: false,
+        newItemInputFocus: false,
         products: this.props.products,
         addedReceptName: "",
         expanded: false,
@@ -11,10 +13,12 @@ class CreateRecept extends Component {
         diner: false,
         tea: false,
         supper: false,
-        addedProducts: [{ name: "", measure: "", kcal: "", number: "1" }],
+        addedProducts: [],
+        newProduct: { name: "", measure: "", kcal: "", number: "" },
         addedProductIndex: -1,
         addedRecept: ""
     }
+
     handleShowCheckboxes = () => {
         this.setState({
             expanded: !this.state.expanded
@@ -53,24 +57,35 @@ class CreateRecept extends Component {
 
     handleAddProduct = (e) => {
         e.preventDefault();
-        const item = { name: "", measure: "", kcal: "", number: "1" };
-        const newList = this.state.addedProducts;
-        newList.push(item);
-        this.setState({
-            addedProducts: newList
-        })
+        if (this.state.products.filter(produkt => produkt.name === this.state.newProduct.name).length === 1) {
+            const newProduct = this.state.products.filter(produkt => produkt.name === this.state.newProduct.name)[0];
+            let addedProducts = this.state.addedProducts
+            addedProducts.push(newProduct)
+            console.log(newProduct)
+            this.setState({
+                addedProducts,
+                newProduct: { name: "", measure: "", kcal: "", number: "" },
+                newItemInputFocus: false,
+                itemSelected: false
+            })
+        } else {
+            alert("Podaj nazwę produktu i wybierz produkt z listy")
+            this.setState({
+                newItemInputFocus: true
+            })
+        }
     }
 
     handleChangeExactProduct = (e) => {
-        let arr = this.state.addedProducts
+        let obj = this.state.newProduct
         if (e.target.type === "text") {
-            arr[e.target.id].name = e.target.value;
+            obj.name = e.target.value;
         } else if (e.target.type === "number") {
-            if (e.target.value < 1) return
-            arr[e.target.id].number = e.target.value;
+            if (e.target.value < 0) return
+            obj.number = e.target.value;
         }
         this.setState({
-            addedProducts: arr
+            newProduct: obj
         })
     }
 
@@ -91,6 +106,18 @@ class CreateRecept extends Component {
 
     handleConfirmAddedRecept = (e) => {
         e.preventDefault();
+        if (this.state.addedReceptName === "") {
+            alert("Nie podałeś nazwy przepisu")
+            return
+        }
+        if (this.state.addedProducts.length === 0) {
+            alert("Nie dodałeś żadnych produktów")
+            return
+        }
+        if (this.state.addedRecept === "") {
+            alert("Nie podałeś przepisu")
+            return
+        }
         const components = this.state.addedProducts;
         let kcal = 0;
         components.forEach(component => kcal += component.kcal * component.number)
@@ -122,7 +149,7 @@ class CreateRecept extends Component {
         })
     }
 
-    handleSelectProduct = (e) => { //po wyborze produktu, kiedy wpisuję kolejne litery, program zachowuje się tak jakby szukał produktu, ale nie ma prawa bo nie ma takiej nazwy w liście
+    handleSelectProduct = (e) => {
         const arr = this.state.addedProducts;
         const element = this.state.products.filter(product => product.name.toLowerCase() === e.target.value.toLowerCase())
         element[0].number = 1
@@ -130,6 +157,35 @@ class CreateRecept extends Component {
         this.setState({
             addedProducts: arr,
             addedProductIndex: -1
+        })
+    }
+
+    handleFocus = () => {
+        this.setState({
+            newItemInputFocus: true
+        })
+    }
+
+    handleBlur = () => {
+        setTimeout(() => {
+            this.setState({
+                newItemInputFocus: false
+            })
+        }, 200);
+    }
+
+    handleSelectProduct = (e) => {
+        const newProduct = this.state.products.filter(produkt => produkt.name === e.target.value)[0]
+        const alreadyExist = this.state.addedProducts.filter(produkt => produkt.name === e.target.value)
+        if (alreadyExist.length > 0) {
+            alert("Dodałeś już ten produkt");
+            return
+        }
+        newProduct.number = 100;
+        this.setState({
+            newProduct,
+            newItemInputFocus: false,
+            itemSelected: true
         })
     }
 
@@ -144,7 +200,7 @@ class CreateRecept extends Component {
     }
 
     render() {
-        const inputSearch = this.state.addedProductIndex === -1 ? null : this.state.products.filter(product => product.name.toLowerCase().includes(this.state.addedProducts[this.state.addedProductIndex].name.toLowerCase()));
+        const inputSearch = this.state.newProduct.name.length < 2 ? null : this.state.products.filter(product => product.name.toLowerCase().includes(this.state.newProduct.name.toLowerCase()));
         let kcal = 0;
         this.state.addedProducts.forEach(product => kcal += product.kcal * product.number)
         return (
@@ -173,17 +229,20 @@ class CreateRecept extends Component {
 
                 <ul className="addProductsList">{this.state.addedProducts.map((product, index) => (
                     <li className="addProductsItem" key={index}>
-                        <input className="addProductsItemName" id={index} onChange={this.handleChangeExactProduct} onFocus={this.state.addedProductIndex !== index ? () => { this.handleSetIndex(index) } : null} type="text" value={product.name} />
-                        <div>{this.state.addedProductIndex === this.handleCheckProductIndex(index) && this.state.addedProducts[this.state.addedProductIndex].name.length > 2 ? <ul>{inputSearch.map((product, index) => <li key={index}><button onClick={(e) => this.handleSelectProduct(e)} value={product.name}>{product.name}</button></li>)}</ul> : null}</div>
-                        <input className="addProductsItemNumber" id={index} onChange={this.handleChangeExactProduct} type="number" value={product.number} /><p>{product.measure}</p>
+                        <h3 className="addProductsItemName" id={index}>{product.name.toUpperCase()}</h3>
+                        <h3 className="addProductsItemNumber" id={index}>{product.number} {product.measure}</h3>
                         <button className="createReceptButtons" id={index} onClick={this.handleDeleteExactProduct}>usuń</button>
                     </li>))}
                 </ul>
 
+                <input className="addProductsItemName" onChange={this.state.itemSelected ? null : this.handleChangeExactProduct} onFocus={this.handleFocus} onBlur={this.handleBlur} type="text" value={this.state.newProduct.name} placeholder="podaj nazwę produktu" />
+                {this.state.newProduct.name.length > 2 && this.state.newItemInputFocus ? <div className="addProductsAutocompleteList">{inputSearch.map((product, index) => <button key={index} onClick={this.handleSelectProduct} value={product.name}>{product.name}</button>)}</div> : null}
+                <input className="addProductsItemNumber" onChange={this.handleChangeExactProduct} type="number" value={this.state.newProduct.number} /><p>{this.state.newProduct.measure}</p>
                 <button className="createReceptButtons" onClick={this.handleAddProduct}>dodaj produkt</button>
 
-                <textarea className="addReceptTextField" value={this.state.addedRecept} onChange={this.handleAddedRecept} cols="40" rows="5"></textarea>
-                <p>kcal: {kcal}</p>
+                <textarea className="addReceptTextField" value={this.state.addedRecept} onChange={this.handleAddedRecept} cols="40" rows="5" placeholder="wprowadź przepis"></textarea>
+                <p>kcal: {Math.round(Math.round(kcal * Math.pow(10, 2 + 1)) / 10) / (Math.pow(10, 2 + 1) / 10)}</p>{//zaokrągla kalorie do 2 miejsc po przecinku
+                }
 
                 <button className="createReceptButtons" onClick={this.handleConfirmAddedRecept}>GOTOWE - DODAJ PRZEPIS</button>
             </div>
