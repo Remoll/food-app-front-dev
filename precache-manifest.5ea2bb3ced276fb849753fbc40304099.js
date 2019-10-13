9c419a489401209e91003e1c26be7b39321b317e
Remoll <remigiusz.slusarczyk@gmail.com>
2019-10-13 22:32:15 +0200

    Updates

---
+++ precache-manifest.5ea2bb3ced276fb849753fbc40304099.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512f1bbaa3264ece173b836ce47f2204",
+    "url": "/food-app-front-dev/index.html"
+  },
+  {
+    "revision": "8ae4f7d81ec26c5ca5ce",
+    "url": "/food-app-front-dev/static/css/main.314f58c8.chunk.css"
+  },
+  {
+    "revision": "3752822ae12bd32e08a7",
+    "url": "/food-app-front-dev/static/js/2.f63b5b6a.chunk.js"
+  },
+  {
+    "revision": "8ae4f7d81ec26c5ca5ce",
+    "url": "/food-app-front-dev/static/js/main.1da59894.chunk.js"
+  },
+  {
+    "revision": "a0a02bad94af0b7d1905",
+    "url": "/food-app-front-dev/static/js/runtime~main.3c10fb19.js"
+  }
+]);
