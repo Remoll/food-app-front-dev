1f4a84bbbaf04c9385021b5a4c65dc739ec1f381
Remoll <remigiusz.slusarczyk@gmail.com>
2019-10-19 20:58:05 +0200

    Updates

--- precache-manifest.108903ec4928d9a8d5f3dfa4f9d8696b.js
+++ precache-manifest.108903ec4928d9a8d5f3dfa4f9d8696b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "564427b22d97bba1a800e0fbf39fc246",
+    "revision": "e41b35315331457c7594b47eeee55f5a",
     "url": "/food-app-front-dev/index.html"
   },
   {
-    "revision": "f5d8ce73308de0d78ddc",
+    "revision": "a87e878ea6ab2235da60",
     "url": "/food-app-front-dev/static/css/main.136a4fe4.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/food-app-front-dev/static/js/2.3738ced2.chunk.js"
   },
   {
-    "revision": "f5d8ce73308de0d78ddc",
-    "url": "/food-app-front-dev/static/js/main.efb697ec.chunk.js"
+    "revision": "a87e878ea6ab2235da60",
+    "url": "/food-app-front-dev/static/js/main.af5b3497.chunk.js"
   },
   {
     "revision": "a0a02bad94af0b7d1905",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/food-app-front-dev/precache-manifest.9fe485042b5421ba3d1e4dc7eee266fc.js"
+  "/food-app-front-dev/precache-manifest.108903ec4928d9a8d5f3dfa4f9d8696b.js"
 );
 
 self.addEventListener('message', (event) => {
