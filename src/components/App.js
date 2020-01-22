4638c8b5f3cd1a47f10040432d3dc49eac648387
Remoll <remigiusz.slusarczyk@gmail.com>
2020-01-22 08:43:05 +0100

    ad reactrouter redirect for "/"

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Link, Switch, Redirect } from 'react-router-dom';
 import { FaAppleAlt, FaDrumstickBite, FaReceipt, FaShoppingCart, } from "react-icons/fa";
 import '../style/style.scss';
 import Recepts from './Recepts';
@@ -83,15 +83,12 @@ class App extends Component {
               <NavLink to="/food-app-front-dev/shopinglist" className='app__main-menu-element'><FaShoppingCart className="app__main-menu-icon" />Zakupy</NavLink>
             </nav>
 
-            <Link to="/food-app-front-dev/"></Link>
-
-
             <div className="app__selected-option">
               <Switch>
                 <Route path="/food-app-front-dev/recepts" render={(props) => <Recepts {...props} serverAdress={this.state.serverAdress} upgradeRecepts={this.upgradeRecepts.bind(this)} setShopingListActiveItem={this.setShopingListActiveItem.bind(this)} products={this.state.products} recepts={this.state.recepts} />} />
                 <Route path="/food-app-front-dev/menu" render={(props) => <Menus {...props} serverAdress={this.state.serverAdress} upgradeMenus={this.upgradeMenus.bind(this)} setShopingListActiveItem={this.setShopingListActiveItem.bind(this)} recepts={this.state.recepts} menus={this.state.menus} />} />
                 <Route path="/food-app-front-dev/shopinglist" render={(props) => <ShopingList {...props} upgradeMenus={this.upgradeMenus.bind(this)} item={this.state.shopingListActiveItem} />} />
-                <Route path="/food-app-front-dev">Użyj menu do nawigacji</Route>
+                <Route path="/" render={() => (<Redirect to="/food-app-front-dev/recepts" />)} />
               </Switch>
             </div>
 
